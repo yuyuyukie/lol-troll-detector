ef1e774f3b3d5362b38cfefd0621b26d6f7fdddf
YukiYama <zx275fire@gmail.com>
2023-01-28 16:47:11 +0900

    fix: fixed type to pass all type-check

--- packages/backend/src/scalars/date.scalar.ts
+++ packages/backend/src/scalars/date.scalar.ts
@@ -1,15 +1,21 @@
 import { CustomScalar, Scalar } from "@nestjs/graphql";
 import { Kind, ValueNode } from "graphql";
 
-@Scalar("Date", (type) => Date)
+@Scalar("Date", (_type) => Date)
 export class DateScalar implements CustomScalar<number, Date> {
   description = "Date custom scalar type";
 
-  serialize(value: Date): number {
+  serialize(value: unknown): number {
+    if (!(value instanceof Date)) {
+      throw new Error("value is not Date object");
+    }
     return value.getTime() / 1000; // value sent to the client
   }
 
-  parseValue(value: number): Date {
+  parseValue(value: unknown): Date {
+    if (typeof value !== "number") {
+      throw new Error("value is not number");
+    }
     return new Date(value); // value from the client
   }
 
