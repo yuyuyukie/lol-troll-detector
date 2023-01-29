df896041c674def8b38b55a8d79af0217ee5c9a2
YukiYama <zx275fire@gmail.com>
2023-01-29 17:54:32 +0900

    delete: graphql

--- packages/backend/src/modules/app.module.ts
+++ packages/backend/src/modules/app.module.ts
@@ -1,23 +1,9 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
-import { GraphQLModule } from "@nestjs/graphql";
-
-import { CategoryModule } from "@/modules/category.module";
-import { TaskModule } from "@/modules/task.module";
-import { TaskContentModule } from "@/modules/taskContent.module";
-import { DateScalar } from "@/scalars/date.scalar";
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    GraphQLModule.forRoot({
-      autoSchemaFile: "schema.graphql",
-    }),
-    TaskModule,
-    TaskContentModule,
-    CategoryModule,
-  ],
+  imports: [ConfigModule.forRoot({ isGlobal: true })],
   controllers: [],
-  providers: [DateScalar],
+  providers: [],
 })
 export class AppModule {}
--- packages/frontend/pages/_app.tsx
+++ packages/frontend/pages/_app.tsx
@@ -1,19 +1,16 @@
 import React from "react";
 
-import { ApolloProvider } from "@apollo/client";
 import { AppProps } from "next/app";
 
-import { client } from "@/graphql/client";
-import { GlobalStyle } from "@/styles/GlobalStyle";
-
 import "semantic-ui-css/semantic.min.css";
+import { GlobalStyle } from "../styles/GlobalStyle";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ApolloProvider client={client}>
+    <>
       {GlobalStyle}
       <Component {...pageProps} />
-    </ApolloProvider>
+    </>
   );
 }
 
